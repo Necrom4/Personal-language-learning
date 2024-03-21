4acc58d7c39ac14f7be519a63af8c2a80e7d4945
Diogo Ferreira Pombeiro <dferreir@c3r3s2.42lausanne.ch>
2024-03-21 15:43:52 +0100

    better game + solved back paddle bounce issue

--- Pong/pong.js
+++ Pong/pong.js
@@ -1,61 +1,69 @@
-let player_y1 = canvas.height / 2 - 100;
-let player_y2 = canvas.height / 2 - 100;
-let player_speed_y1 = 0;
-let player_speed_y2 = 0;
-let ball_x = canvas.width / 2;
-let ball_y = canvas.height / 2;
+let paddle_y1 = paddle_y2 = canvas.height / 2 - 100;
+let paddle_speed_y1 = 0;
+let paddle_speed_y2 = 0;
 let ball_length = 30;
+let ball_x = canvas.width / 2 - ball_length / 2;
+let ball_y = canvas.height / 2 - ball_length / 2;
 let ball_speed = 400;
 let ball_angle = ((Math.random() < 0.5 ? 1 : -1) == true) ? (Math.PI - ((Math.random() * (225 * Math.PI / 180 - 135 * Math.PI / 180)) + 135 * Math.PI / 180)) : ((Math.random() * (225 * Math.PI / 180 - 135 * Math.PI / 180)) + 135 * Math.PI / 180);
 var score = [0, 0];
-let first_hit = 0;
+let paddle_bounces = 0;
 let last_frame;
 let wait_frames = 0;
+let speed_acc = 3;
+let n = 0;
 
-function bounceAngle(ball_angle, ball_x, ball_y, ball_length, player_y1, player_y2) {
-	let player;
+function bounceAngle(ball_angle, ball_x, ball_y, ball_length, paddle_y1, paddle_y2) {
+	let paddle;
+	paddle_bounces++;
 	if (ball_angle > Math.PI / 2 && ball_angle < Math.PI + Math.PI / 2)
-		player = player_y1;
+		paddle = paddle_y1;
 	else if (ball_angle < Math.PI / 2 || ball_angle > Math.PI + Math.PI / 2)
-		player = player_y2;
-	let relative_position = (ball_y + ball_length / 2) - (player + 100);
+		paddle = paddle_y2;
+	let relative_position = (ball_y + ball_length / 2) - (paddle + 100);
 
-	if (player == player_y1)
-		return (2 * Math.PI - ((360 - relative_position * 65 / 100) * Math.PI / 180));
-	else if (player == player_y2)
-		return (2 * Math.PI - ((180 + relative_position * 65 / 100) * Math.PI / 180));
+	if (paddle == paddle_y1)
+		return (2 * Math.PI - ((360 - relative_position * 45 / 100) * Math.PI / 180));
+	else if (paddle == paddle_y2)
+		return (2 * Math.PI - ((180 + relative_position * 45 / 100) * Math.PI / 180));
 }
 
 function updatePos(time_diff) {
-	wait_frames--
+	wait_frames--;
+	frames++;
 	if (wait_frames == 1) {
-		player_speed_y1 = 0;
-		player_speed_y2 = 0;
-		ball_x = canvas.width / 2;
-		ball_y = canvas.height / 2;
+		paddle_speed_y1 = 0;
+		paddle_speed_y2 = 0;
+		ball_x = canvas.width / 2 - ball_length / 2;
+		ball_y = canvas.height / 2 - ball_length / 2;
 		ball_speed = 400;
 		ball_angle = ((Math.random() < 0.5 ? 1 : -1) == true) ? (Math.PI - ((Math.random() * (225 * Math.PI / 180 - 135 * Math.PI / 180)) + 135 * Math.PI / 180)) : ((Math.random() * (225 * Math.PI / 180 - 135 * Math.PI / 180)) + 135 * Math.PI / 180);
-		first_hit = 0;
+		paddle_bounces = 0;
+		speed_acc = 3;
+		frames = 0;
 	}
 
-  player_y1 += player_speed_y1 * time_diff;
-  player_y2 += player_speed_y2 * time_diff;
-	if (player_y1 < 10 || player_y1 > 955)
-		player_y1 -= player_speed_y1 * time_diff;
-	if (player_y2 < 10 || player_y2 > 955)
-		player_y2 -= player_speed_y2 * time_diff;
+  paddle_y1 += paddle_speed_y1 * time_diff;
+  paddle_y2 += paddle_speed_y2 * time_diff;
+	if (paddle_y1 < 10 || paddle_y1 > 955)
+		paddle_y1 -= paddle_speed_y1 * time_diff;
+	if (paddle_y2 < 10 || paddle_y2 > 955)
+		paddle_y2 -= paddle_speed_y2 * time_diff;
 
 	ball_x += ball_speed * Math.cos(ball_angle) * time_diff;
 	ball_y += ball_speed * Math.sin(ball_angle) * time_diff;
 	if (ball_y < 0 || ball_y > canvas.height - ball_length)
 		ball_angle = 2 * Math.PI - ball_angle;
-	if ((ball_y >= player_y1 - 25 && ball_y <= player_y1 + 200 && ball_x <= 50 && ball_angle > Math.PI / 2 && ball_angle < Math.PI * 1.5) || (ball_y >= player_y2 - 25 && ball_y <= player_y2 + 200 && ball_x + ball_length>= 1130 && (ball_angle < Math.PI / 2 || ball_angle > Math.PI * 1.5))) {
-		// ball_angle = Math.PI - ball_angle;
-		ball_angle = bounceAngle(ball_angle, ball_x, ball_y, ball_length, player_y1, player_y2);
-		if (!first_hit) {
-			first_hit = 1;
+	if ((ball_y >= paddle_y1 - 25 && ball_y <= paddle_y1 + 200 && ball_x <= 50 && ball_x >= 20 && ball_angle > Math.PI / 2 && ball_angle < Math.PI * 1.5) || (ball_y >= paddle_y2 - 25 && ball_y <= paddle_y2 + 200 && ball_x + ball_length >= 1130 && ball_x <= 1160 && (ball_angle < Math.PI / 2 || ball_angle > Math.PI * 1.5))) {
+		ball_angle = bounceAngle(ball_angle, ball_x, ball_y, ball_length, paddle_y1, paddle_y2);
+		if (Math.floor(Math.random() * speed_acc) == 1)
+			ball_speed *= 1.1;
+		if (ball_speed > 1500)
+			speed_acc = 10;
+		if (ball_speed > 2000)
+			ball_speed = 2000;
+		if (paddle_bounces == 1)
 			ball_speed *= 2;
-		}
 	}
 	if (ball_x < -ball_length && wait_frames < 0) {
 		score[1]++;
@@ -68,6 +76,11 @@ function updatePos(time_diff) {
 }
 
 function draw() {
+	if (!n) {
+		paddle_y2 += 2;
+		paddle_y2--;
+	}
+	n++;
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -79,9 +92,13 @@ function draw() {
 	ctx.textAlign = "center";
 	ctx.fillText(score[0].toString(), canvas.width / 3, 100);
 	ctx.fillText(score[1].toString(), canvas.width - canvas.width / 3, 100);
+	ctx.font = "20px Courier New";
+	ctx.textAlign = "left";
+	ctx.fillText("Ball Speed: " + Math.floor(ball_speed), 20, 30);
+	ctx.fillText("Paddle Bounces: " + paddle_bounces, 20, 50);
   ctx.fillStyle = "rgb(255, 255, 255)";
-  ctx.fillRect(20, player_y1, 30, 200);
-  ctx.fillRect(1130, player_y2, 30, 200);
+  ctx.fillRect(20, paddle_y1, 30, 200);
+  ctx.fillRect(1130, paddle_y2, 30, 200);
 	ctx.beginPath();
 	ctx.fillRect(ball_x, ball_y, ball_length, ball_length);
 	ctx.fill();
@@ -99,20 +116,20 @@ function loop(current_frame) {
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "w")
-    player_speed_y1 = -1000;
+    paddle_speed_y1 = -1000;
   else if (event.key === "s")
-    player_speed_y1 = 1000;
+    paddle_speed_y1 = 1000;
   else if (event.key === "ArrowUp")
-    player_speed_y2 = -1000;
+    paddle_speed_y2 = -1000;
   else if (event.key === "ArrowDown")
-    player_speed_y2 = 1000;
+    paddle_speed_y2 = 1000;
 });
 
 document.addEventListener("keyup", (event) => {
   if (event.key === "w" || event.key === "s") {
-    player_speed_y1 = 0;
+    paddle_speed_y1 = 0;
   } else if (event.key === "ArrowUp" || event.key === "ArrowDown") {
-    player_speed_y2 = 0;
+    paddle_speed_y2 = 0;
   }
 });
 
