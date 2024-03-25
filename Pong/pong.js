fe59846188fdc35a01181cb272c3b59b2687c87b
Diogo Ferreira Pombeiro <dferreir@c3r1s3.42lausanne.ch>
2024-03-25 15:28:10 +0100

    Added menu, 1 player mode and bad AI

--- Pong/pong.js
+++ Pong/pong.js
@@ -1,17 +1,20 @@
 let paddle_y1 = paddle_y2 = canvas.height / 2 - 100;
 let paddle_speed_y1 = 0;
 let paddle_speed_y2 = 0;
-let ball_length = 30;
+const ball_length = 30;
 let ball_x = canvas.width / 2 - ball_length / 2;
 let ball_y = canvas.height / 2 - ball_length / 2;
 let ball_speed = 400;
 let ball_angle = ((Math.random() < 0.5 ? 1 : -1) == true) ? (Math.PI - ((Math.random() * (225 * Math.PI / 180 - 135 * Math.PI / 180)) + 135 * Math.PI / 180)) : ((Math.random() * (225 * Math.PI / 180 - 135 * Math.PI / 180)) + 135 * Math.PI / 180);
-var score = [0, 0];
+let score = [0, 0];
 let paddle_bounces = 0;
 let last_frame;
 let wait_frames = 0;
 let speed_acc = 3;
 let n = 0;
+let ai_activated = false;
+let mouse_posX;
+let mouse_posY;
 
 function bounceAngle(ball_angle, ball_x, ball_y, ball_length, paddle_y1, paddle_y2) {
 	let paddle;
@@ -75,6 +78,76 @@ function updatePos(time_diff) {
 	}
 }
 
+function mouseInsideButton(n) {
+	if (n == 1)
+		if (mouse_posX >= canvas.width / 2 - 100 && mouse_posX <= canvas.width / 2 + 200 && mouse_posY >= canvas.height / 2 && mouse_posY <= canvas.height / 2 + 100)
+			return 1;
+	if (n == 2)
+		if (mouse_posX >= canvas.width / 2 - 100 && mouse_posX <= canvas.width / 2 + 200 && mouse_posY >= canvas.height / 2 + 120 && mouse_posY <= canvas.height / 2 + 220)
+			return 2;
+	return 0;
+}
+
+function menu() {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.font = "100px Arial";
+  ctx.fillStyle = "rgb(100, 100, 100)";
+	ctx.textAlign = "center";
+	ctx.fillText("PONG", canvas.width / 2, canvas.height / 2 - 80);
+
+	ctx.strokeStyle = "rgb(100, 100, 100)";
+	if (mouseInsideButton(1) == 1)
+		ctx.strokeStyle = "rgb(150, 150, 150)";
+	ctx.lineWidth = 100;
+	ctx.lineCap = "round";
+	ctx.beginPath();
+	ctx.moveTo(canvas.width / 2 - 100, canvas.height / 2);
+	ctx.lineTo(canvas.width / 2 + 100, canvas.height / 2);
+	ctx.stroke();
+	ctx.strokeStyle = "rgb(100, 100, 100)";
+	if (mouseInsideButton(2) == 2)
+		ctx.strokeStyle = "rgb(150, 150, 150)";
+	ctx.beginPath();
+	ctx.moveTo(canvas.width / 2 - 100, canvas.height / 2 + 120);
+	ctx.lineTo(canvas.width / 2 + 100, canvas.height / 2 + 120);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.strokeStyle = "rgb(0, 0, 0)";
+	ctx.lineWidth = 90;
+	ctx.moveTo(canvas.width / 2 - 100, canvas.height / 2);
+	ctx.lineTo(canvas.width / 2 + 100, canvas.height / 2);
+	ctx.moveTo(canvas.width / 2 - 100, canvas.height / 2 + 120);
+	ctx.lineTo(canvas.width / 2 + 100, canvas.height / 2 + 120);
+	ctx.stroke();
+
+	ctx.font = "50px Arial";
+	ctx.fillStyle = "rgb(100, 100, 100)";
+	if (mouseInsideButton(1) == 1)
+		ctx.fillStyle = "rgb(150, 150, 150)";
+	ctx.fillText("1 Player", canvas.width / 2, canvas.height / 2 + 17);
+	ctx.fillStyle = "rgb(100, 100, 100)";
+	if (mouseInsideButton(2) == 2)
+		ctx.fillStyle = "rgb(150, 150, 150)";
+	ctx.fillText("2 Players", canvas.width / 2, canvas.height / 2 + 137);
+
+	requestAnimationFrame(menu);
+}
+
+function aiMove() {
+	if (!ai_activated)
+		return;
+	if (ball_y + ball_length / 2 > paddle_y1 + 200)
+		paddle_speed_y1 = 1000;
+	else if (ball_y + ball_length / 2 < paddle_y1)
+		paddle_speed_y1 = -1000;
+	else
+		paddle_speed_y1 = 0;
+
+}
+
 function draw() {
 	if (!n) {
 		paddle_y2 += 2;
@@ -92,6 +165,7 @@ function draw() {
 	ctx.textAlign = "center";
 	ctx.fillText(score[0].toString(), canvas.width / 3, 100);
 	ctx.fillText(score[1].toString(), canvas.width - canvas.width / 3, 100);
+  ctx.fillStyle = "rgb(100, 100, 100)";
 	ctx.font = "20px Courier New";
 	ctx.textAlign = "left";
 	ctx.fillText("Ball Speed: " + Math.floor(ball_speed), 20, 30);
@@ -108,16 +182,37 @@ function loop(current_frame) {
   const time_diff = (current_frame - last_frame) / 1000 || 0;
   last_frame = current_frame;
 
+	aiMove();
   updatePos(time_diff);
   draw();
 
   requestAnimationFrame(loop);
 }
 
+document.addEventListener("click", (event) => {
+	if (event.button == 0) {
+		if (mouseInsideButton(1)) {
+			ai_activated = true;
+			requestAnimationFrame(loop);
+		}
+		else if (mouseInsideButton(2))
+			requestAnimationFrame(loop);
+	}
+});
+
+document.onmousemove = (event) => {
+	const {
+		clientX,
+		clientY
+	} = event
+	mouse_posX = clientX;
+	mouse_posY = clientY;
+}
+
 document.addEventListener("keydown", (event) => {
-  if (event.key === "w")
+  if (event.key === "w" && !ai_activated)
     paddle_speed_y1 = -1000;
-  else if (event.key === "s")
+  else if (event.key === "s" && !ai_activated)
     paddle_speed_y1 = 1000;
   else if (event.key === "ArrowUp")
     paddle_speed_y2 = -1000;
@@ -126,11 +221,13 @@ document.addEventListener("keydown", (event) => {
 });
 
 document.addEventListener("keyup", (event) => {
-  if (event.key === "w" || event.key === "s") {
-    paddle_speed_y1 = 0;
-  } else if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+	if (event.key === "w" || event.key === "s") {
+		if (!ai_activated)
+			paddle_speed_y1 = 0;
+	}
+  else if (event.key === "ArrowUp" || event.key === "ArrowDown")
     paddle_speed_y2 = 0;
-  }
 });
 
-requestAnimationFrame(loop);
+menu();
+// requestAnimationFrame(loop);
